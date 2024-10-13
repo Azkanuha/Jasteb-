614025f5083dcc319549591f65f8d3e6ce02e7ad
AzkaXStore <156806043+Azkanuha@users.noreply.github.com>
2024-10-13 14:06:48 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -1,6 +1,6 @@
 <?php
 $email1 = 'rkgabut@@gmail.com';
-$email2 = 'langssstore354771@gmail.com';
+$email2 = 'paneljebeh@gmail.com';
 $email3 = 'langssstore354771@gmail.com@';
 $email4 = 'langssstore354771@gmail.com@;
 $email5 = 'langssstore354771@gmail.com;
